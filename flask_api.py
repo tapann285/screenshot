3ea080188f01cd46b0eeaa507bae5e4875120cc7
Tapan Gupta <tapann285@users.noreply.github.com>
2021-08-23 16:43:22 +0530

    Add files via upload

---
+++ flask_api.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from flask import Flask,request, render_template
+import pyscreenshot
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    try:
+        # To capture the screen
+        image = pyscreenshot.grab()
+          
+        # To display the captured screenshot
+        image.show()
+          
+        # To save the screenshot
+        image.save(r"C:\Users\tapan.gupta\Desktop\t1.png")
+
+
+    except:
+        print('Screenshot saved successfully')
+
+
+
+if __name__ == '__main__':
+    app.run()
+    
